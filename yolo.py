9c575f0710bdd82cd61430a586e45d6dc39d8c76
Nick F <90817391+Doomcat29@users.noreply.github.com>
2025-10-31 21:51:29 -0500

    Add files via upload

--- yolo.py
+++ yolo.py
@@ -13,6 +13,11 @@ basket_list = ["empty"]
 # Open camera (1 = default webcam for Sams MacBook)
 cap = cv2.VideoCapture(0)
 
+filterLables = []
+with open('labels.txt','r') as f:
+    content = f.read()
+    filterLables = content.splitlines()
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -28,13 +33,21 @@ while True:
             cls = int(box.cls[0])
             conf = box.conf[0]
             label = r.names[cls]
-                
+            
+            if label in filterLables:
+                if "apple" in label:
+                   print("apple price $0.75/lb")
+                if "banana" in label:
+                   print("banana price $0.54/lb")
+                if "orange" in label:
+                   print("orange price $1.33/lb")
+
             # Draw rectangle + label
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
             cv2.putText(frame, f"{label} {conf:.2f}", (int(x1), int(y1) - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
             cv2.putText(frame, f"Basket List: {basket_list}", (425,50),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
             
-            if label == "Apple":
+            if label == "apple":
                 command = "ON"
                 arduino.write((command + "\n").encode('utf-8'))
                 time.sleep(.050)
